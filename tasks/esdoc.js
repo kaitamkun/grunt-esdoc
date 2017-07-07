09d87d30fc3d7e8eeda7d8d394de97f98e494c4a
Kai Tamkun <ktamkun@ucsc.edu>
2017-07-07 01:46:01 -0700

    Restyled and updated for esdoc 0.5.2.

--- tasks/esdoc.js
+++ tasks/esdoc.js
@@ -1,91 +1,61 @@
-/*
-* grunt-my-plugin
-*
-*
-* Copyright (c) 2015
-* Licensed under the MIT license.
-*/
-
 "use strict";
 
-var esdoc = require(`esdoc`);
-var publisher = require(`esdoc/out/src/Publisher/publish`);
+const esdoc = require("esdoc");
+const publisher = require("esdoc/out/src/Publisher/publish");
 
 module.exports = function (grunt) {
-
-    grunt.registerMultiTask(`esdoc`, `ES6 Documentation tool ESDoc Grunt Plugin`, function () {
-
+    grunt.registerMultiTask("esdoc", "Grunt plugin for ESDoc", function () {
         // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({
-            coverage: true
-        });
+        const options = this.options({ coverage: true });
+        let config = options;
 
-        var config = {};
-
-        if (typeof options.config !== `undefined`) {
+        if (typeof options.config != "undefined") {
             config = grunt.file.readJSON(options.config);
-        } else {
-            config = options;
         }
 
-        // Destination must be set
-        if (typeof config.destination !== `string`) {
-            grunt.fail.fatal(`ESDoc requires "destination" option be set`);
+        // The destination must be set.
+        if (typeof config.destination != "string") {
+            grunt.fail.fatal(`esdoc requires that the "destination" option be set.`);
         }
 
-        // If grunt.debug is set to true then override whatever the debug
-        // option is set to by any esdoc config
-        if (grunt.option(`debug`) && grunt.option(`verbose`)) {
-            config.debug = true;
-        }
+        // If grunt.debug is set to true, then override whatever the debug option is set to by any esdoc config.
+        config.debug = grunt.option("debug") && grunt.option("verbose") || config.debug;
 
         const oldLog = console.log;
 
         try {
-
-            // Capture console.log to filter output
-            // If in verbose mode then log the lines that are output
+            // Capture console.log() so we can filter esdoc's output. If in verbose mode, log the captured lines.
             const lines = [];
-            console.log = function (msg) {
+
+            console.log = (msg) => {
                 lines.push(msg);
 
-                if (grunt.option(`verbose`)) {
+                if (grunt.option("verbose")) {
                     oldLog.apply(console, arguments);
                 }
             }
 
-            // Pass the config to ESDoc to publish the documentation
-            esdoc.generate(config, publisher);
+            // Pass the config to esdoc to publish the documentation.
+            esdoc.default.generate(config, publisher.default);
 
-            // If coverage is configured then parse out the coverage values
-            // This goes in reverse (as coverage is near the end) and uses
-            // some to iterate over the reversed lines until one matching
-            // the regex is found
+            // If coverage is configured, then parse out the coverage values.
+            // This goes in reverse (as coverage is near the end) and uses some to iterate over the reversed lines until a matching one is found.
             lines.reverse().some((ln, m) => {
                 if (m = ln.match(/^Coverage:\s*(\d{1,3}(?:\.\d{0,2}))%\s*\((\d+)\/(\d+)\)$/)) {
-                    const percent = parseFloat(m[1]);
-                    const amount = parseInt(m[2]);
-                    const total = parseInt(m[3]);
-
-                    grunt.log[percent < 100 ? `warn` : `ok`](`Coverage: ${percent}%`);
-                    grunt.log[amount < total ? `warn` : `ok`](`Files: ${amount}/${total}`);
+                	const [percent, amount, total] = [1, 2, 3].map((n) => parseFloat(m[n]));
+                    grunt.log[percent < 100?   "warn" : "ok"](`Coverage: ${percent}%`);
+                    grunt.log[ amount < total? "warn" : "ok"](`Files: ${amount}/${total}`);
                 }
+
                 return m;
             });
-
         } catch (err) {
-
-            // Fail on any error
-            // Note that this will not reassign console.log as the entire
-            // grunt process will exit here
+        	console.log = oldLog;
+            // Fail on any error.
             grunt.fail.fatal(err);
-
         } finally {
-
-            // Return control of console.log to normal
+            // Reset console.log.
             console.log = oldLog;
-
         }
     });
-
 };
